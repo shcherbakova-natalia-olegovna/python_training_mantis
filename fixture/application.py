be032b7e241a3f28625b852472dc384de633f5a4
shcherbakova-natalia-olegovna <NataliaShcherbakovaAscon@gmail.com>
2019-01-07 20:10:15 +0300

    Новое учебное приложение: баг-трекер Mantis.

---
+++ fixture/application.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from fixture.session import SessionHelper
+
+
+
+class Application:
+
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+        self.wd.implicitly_wait(2)
+        self.session = SessionHelper(self)
+        self.base_url = base_url
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
+
+    def open_home_page(self):
+        wd = self.wd
+        if not (wd.current_url.endswith("addressbook/") and len(wd.find_elements_by_name("add")) > 0):
+            wd.get(self.base_url)
+
+    def return_to_home_page(self):
+        wd = self.wd
+        if not (wd.current_url.endswith("addressbook/") and len(wd.find_elements_by_name("add")) > 0):
+            wd.find_element_by_id("logo").click()
+
+    def destroy(self):
+        self.wd.quit()
+
